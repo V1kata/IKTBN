e38c32967f336a6a686af2910ca0215c37114f81
V1kata <viktor.burboran@gmail.com>
2025-10-20 13:44:32 +0300

    added the new component

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Footer from "@/app/ui/footer";
 import Header from "@/app/ui/header";
 import { UserProvider } from "@/app/context/UserContext";
+import KeepProfile from "./ui/middlewares/keepProfile";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,29 +17,31 @@ export default function RootLayout({ children }) {
     <html lang="bg">
       <UserProvider>
         <body className="min-h-screen flex flex-col bg-gradient-to-br from-[#faf8f6] via-[#f5f0eb] to-[#f0ebe7] text-[#2e2e2e] relative overflow-x-hidden">
-          {/* Декоративен елемент */}
-          <div className="absolute inset-0 -z-10 opacity-40 bg-[url('/ornament-bg.svg')] bg-repeat" />
+          <KeepProfile>
+            {/* Декоративен елемент */}
+            <div className="absolute inset-0 -z-10 opacity-40 bg-[url('/ornament-bg.svg')] bg-repeat" />
 
-          <Header />
+            <Header />
 
-          <main className="flex-1 w-full relative z-10 px-6 py-10 flex justify-center items-center">
-            {/* Декоративен фон */}
-            <div
-              className="absolute inset-0 -z-10 bg-gradient-to-b from-[#f8f5f2]/70 to-[#e9dfd5]/70
+            <main className="flex-1 w-full relative z-10 px-6 py-10 flex justify-center items-center">
+              {/* Декоративен фон */}
+              <div
+                className="absolute inset-0 -z-10 bg-gradient-to-b from-[#f8f5f2]/70 to-[#e9dfd5]/70
               before:absolute before:content-[''] before:inset-0
               before:bg-[url('/ornament-bg.svg')] before:opacity-25 before:bg-repeat before:bg-center
               blur-sm"
-            />
+              />
 
-            {/* Съдържанието на страниците */}
-            <div className="w-full max-w-6xl backdrop-blur-sm bg-white/70 p-8 rounded-2xl shadow-lg">
-              {children}
-            </div>
-          </main>
+              {/* Съдържанието на страниците */}
+              <div className="w-full max-w-6xl backdrop-blur-sm bg-white/70 p-8 rounded-2xl shadow-lg">
+                {children}
+              </div>
+            </main>
 
-          <Footer />
+            <Footer />
+          </KeepProfile>
         </body>
       </UserProvider>
-    </html>
+    </html >
   );
 }
