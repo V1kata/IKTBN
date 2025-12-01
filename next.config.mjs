5d24319bea11afb2418e71d9e262145797cb673b
V1kata <viktor.burboran@gmail.com>
2025-12-01 20:22:24 +0200

    config edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    turbo: false, // disables Turbopack
+  },
+};
 
 export default nextConfig;
