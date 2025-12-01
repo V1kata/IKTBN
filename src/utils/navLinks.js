ff6da12a2def6c181db0367bee576af5e712ce25
V1kata <viktor.burboran@gmail.com>
2025-12-01 19:25:34 +0200

    tiny changes to links

--- src/utils/navLinks.js
+++ src/utils/navLinks.js
@@ -2,12 +2,10 @@ import { User, LogOut } from "lucide-react";
 
 export const navLinks = [
     { href: "/", label: "Начало", access: "all" },
-    { href: "/contact", label: "Контакти", access: "all" },
     { href: "/auth/invite", label: "Заяви учител", access: "unloged" },
     { href: "/mylessons", label: "Моите уроци", access: "teacher" },
     { href: "/content/create-lesson", label: "Създай урок", access: "teacher" },
     { href: "/auth/requestTeacher", label: "Заявки за учител", access: "admin" },
     { href: "/auth/login", label: "Вход", access: "unloged" },
-    // { href: "/profile", label: "Профил", access: "loged", icon: <User size={18} /> },
     { href: "/auth/logout", label: "Изход", access: "loged", icon: <LogOut size={18} /> },
   ];
